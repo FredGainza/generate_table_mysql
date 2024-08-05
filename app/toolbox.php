c1d029a2ab45cac1be5cb0119f8c3ce72b4d96eb
KoPaTik <57705953+FredGainza@users.noreply.github.com>
2024-08-05 21:01:18 +0200

    Create toolbox.php
    Ajout du fichier manquant toolbox.php

---
+++ app/toolbox.php
@@ -0,0 +1,48 @@
+<?php
+    function dumpPre($x){
+    echo '<pre>';
+    var_dump($x);
+    echo '</pre>';
+}
+
+function printPre($x){
+    echo '<pre>';
+    print_r($x);
+    echo '</pre>';
+}
+
+// $prenom = valid_donnees($_POST["prenom"]);
+function valid_donnees($donnees){
+    $donnees = trim($donnees);
+    $donnees = stripslashes($donnees);
+    $donnees = htmlspecialchars($donnees);
+    return $donnees;
+}
+
+/* Générateur de Slug (Friendly Url) : convertit un titre en une URL conviviale.*/
+function slugify($string, $delimiter = '-') {
+	$oldLocale = setlocale(LC_ALL, '0');
+	setlocale(LC_ALL, 'en_US.UTF-8');
+	$clean = iconv('UTF-8', 'ASCII//TRANSLIT', $string);
+	$clean = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $clean);
+	$clean = strtolower($clean);
+	$clean = preg_replace("/[\/_|+ -]+/", $delimiter, $clean);
+	$clean = trim($clean, $delimiter);
+	setlocale(LC_ALL, $oldLocale);
+	return $clean;
+}
+
+function tronquer_texte($texte, $nbchar)
+{
+    return (strlen($texte) > $nbchar ? substr(substr($texte,0,$nbchar),0,
+    strrpos(substr($texte,0,$nbchar)," "))." (...)" : $texte);
+}
+
+function enleveaccents($chaine)
+{
+    $search  = array('À', 'Á', 'Â', 'Ã', 'Ä', 'Å', 'Ç', 'È', 'É', 'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï', 'Ò', 'Ó', 'Ô', 'Õ', 'Ö', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'à', 'á', 'â', 'ã', 'ä', 'å', 'ç', 'è', 'é', 'ê', 'ë', 'ì', 'í', 'î', 'ï', 'ð', 'ò', 'ó', 'ô', 'õ', 'ö', 'ù', 'ú', 'û', 'ü', 'ý', 'ÿ' , '-');
+    $replace = array('A', 'A', 'A', 'A', 'A', 'A', 'C', 'E', 'E', 'E', 'E', 'I', 'I', 'I', 'I', 'O', 'O', 'O', 'O', 'O', 'U', 'U', 'U', 'U', 'Y', 'a', 'a', 'a', 'a', 'a', 'a', 'c', 'e', 'e', 'e', 'e', 'i', 'i', 'i', 'i', 'o', 'o', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'u', 'y', 'y' , '' );
+
+    $chaine = str_replace($search, $replace, $chaine);
+    return $chaine; //On retourne le résultat
+}
